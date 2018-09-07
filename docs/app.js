bc6f89148193eeacebf1cf07386a5d2f55b87bc6
Adam Zaninovich <adam.zaninovich@gmail.com>
2018-09-07 16:03:04 -0700

    styles needle and adds big j YEAH

--- docs/app.js
+++ docs/app.js
@@ -14,28 +14,14 @@ function createRain(nbDrop) {
     $('#drop'+i).css('top',dropTop);
   }
 }
-function getWeather(){
-  // Docs
-  // https://www.wunderground.com/weather/api/d/docs?d=resources/phrase-glossary#current_condition_phrases
-  $.ajax('http://api.wunderground.com/api/c6dc8e785d943109/conditions/q/WA/Seattle.json', {
-    dataType: 'jsonp',
-    success: function(json) {
-      if (json.current_observation.weather.toLowerCase().includes("rain")) {
-        createRain(50);
-      } else if (json.current_observation.weather.toLowerCase().includes("rain")){
-        createRain(20);
-      } else {
-        createRain(15);
-      }
-      // $('div#city strong').text(json.current_observation.display_location.full)
-      // $('div#icon').html('<img src=' + json.current_observation.icon_url + '>')
-      $('div#weather').text(json.current_observation.temperature_string + " " + json.current_observation.weather);
-      // $('div#time').text(json.current_observation.observation_time_rfc822);
-    }
-  });
-}
+// Make it rain
+createRain();
 
-jQuery(function($){
-  getWeather();
-  $('#result').fadeIn(1000);
-})
+document.getElementById("YEAH-img").addEventListener("click", function() {
+  img = document.getElementById("YEAH-img")
+  if (img.src.endsWith("images/YEAH.png")) {
+    img.src = "images/jeff.png"
+  } else {
+    img.src = "images/YEAH.png"
+  }
+});
